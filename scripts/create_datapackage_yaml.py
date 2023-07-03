b97781b3d71bc3073945a972a55d51ba639918e2
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-03 11:50:42 -0300

    Cria script para datapackage.yaml, caso ainda não exista

---
+++ scripts/create_datapackage_yaml.py
@@ -0,0 +1,18 @@
+import os
+from frictionless import describe
+
+def create_datapackage_yaml():
+    dataset_files = os.listdir('dataset')
+    dataset_files = os.listdir('dataset/')
+    if 'datapackage.yaml' not in dataset_files:
+        package = describe('dataset/data', type="package")
+        package['name'] = 'nome-pacote'
+        package['owner-org'] = 'organizacao-pacote'
+        for resource in package.resources:
+            resource['title'] = 'Insira um título humanamente legível para este recurso.'
+            resource['description'] = 'Insira a descrição/explicação detalhada deste recurso.'
+
+        package.to_yaml('dataset/datapackage.yaml')
+
+if __name__ == '__main__':
+    create_datapackage_yaml()
