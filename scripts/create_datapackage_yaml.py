72557de8b41a965c6afb4dbad8865eb1622372fd
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-10 14:36:22 -0300

    Melhora caminhos criação datapackage.yaml

--- scripts/create_datapackage_yaml.py
+++ scripts/create_datapackage_yaml.py
@@ -4,12 +4,12 @@ from frictionless import describe
 def create_datapackage_yaml():
     dataset_files = os.listdir('dataset')
     if 'datapackage.yaml' not in dataset_files:
-        package = describe('./data', type='package', basepath='dataset')
+        package = describe('./data', type='package', basepath='./dataset')
         package['name'] = 'nome-pacote'
         package['owner-org'] = 'organizacao-pacote'
         for resource in package.resources:
-            resource['title'] = 'Insira um título humanamente legível para este recurso.'
             resource['description'] = 'Insira a descrição/explicação detalhada deste recurso.'
+            resource['title'] = 'Insira um título humanamente legível para este recurso.'
 
         package.to_yaml('dataset/datapackage.yaml')
 
