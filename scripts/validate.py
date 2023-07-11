70cdf1d8e3e075205aacae4c4e793174020df3fa
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-07-11 17:56:40 -0300

    Fix actions validate error

--- scripts/validate.py
+++ scripts/validate.py
@@ -1,7 +1,7 @@
 from frictionless import validate
 
 def validate_package():
-    report = validate('datapackage.json')
+    report = validate('./dataset/datapackage.json')
     report.to_json('datapackage_validation.json')
 
 if __name__ == '__main__':
