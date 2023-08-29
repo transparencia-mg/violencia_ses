6d87a551e8afa78b3e290ffff3d95d6343e3ce55
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-29 15:36:41 -0300

    Teste 2 abas mesmo arquivo
    See #35

--- scripts/config.py
+++ scripts/config.py
@@ -4,9 +4,9 @@
 # basta descomentar o código abaixo e incluir o nome do arquivo (com a correta extensão)
 # e sua(s) respectiva(s) aba(s) para conversão.
 abas = {
-    # 'nome_do_arquivo.xlsx': [
-    #     'nome_da_aba1',
-    #     'nome_da_aba2',
+    'letters.xlsx': [
+        'teste1',
+        'teste2',
     #     'nome_da_aba3',
-    # ],
+    ],
 }
