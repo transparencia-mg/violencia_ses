09da4c7f9aeb92608a93c9b56c8f9cf6fe671c71
Gabriel Dornas <gabrielbdornas@gmail.com>
2023-08-29 14:46:49 -0300

    Adiciona processo para converter abas
    See #35

---
+++ scripts/config.py
@@ -0,0 +1,12 @@
+# CONFIGURAÇÕES DATASET TEMPLATE
+
+# Abas: caso mais de uma aba em um arquivo excel necessite ser convertida para CSV
+# basta descomentar o código abaixo e incluir o nome do arquivo (com a correta extensão)
+# e sua(s) respectiva(s) aba(s) para conversão.
+abas = {
+    # 'nome_do_arquivo.xlsx': [
+    #     'nome_da_aba1',
+    #     'nome_da_aba2',
+    #     'nome_da_aba3',
+    # ],
+}
