5565290522f7807f6a7f94a66bc4f203fb46c2d4
andrelamor <amorim.andrel@gmail.com>
2023-08-31 20:27:41 -0300

    retorna valores genéricos arquivo config.py

--- scripts/config.py
+++ scripts/config.py
@@ -4,10 +4,9 @@
 # basta descomentar o código abaixo e incluir o nome do arquivo (com a correta extensão)
 # e sua(s) respectiva(s) aba(s) para conversão.
 abas = {
-     	'nome_do_arquivo.xlsx': [
-        'aditivos',
-        'relatorios',
-        'repasses',
-		 'termo-parceria-contrato-gestao'
-     ],
+#     'nome_do_arquivo.xlsx': [
+#         'nome_da_aba1',
+#         'nome_da_aba2',
+#         'nome_da_aba3',
+#     ],
 }
